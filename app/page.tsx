92d630b9079a3887d36a121b521005d23630c711
Ricky <ricky.liu@recordpoint.com>
2024-03-27 11:46:59 +1100

    Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -39,17 +39,17 @@ export default function Home() {
           <ImageDraw fileName={Image} command={query}/>
         }
         <div className="w-10/12 md:w-144 xl:w-160">
-          <textarea autoCapitalize="none" onChange={e => setText(e.target.value)} className="w-full mt-12 mb-4 px-2 pt-2 h-10svh"></textarea>
+          <textarea autoCapitalize="none" onChange={e => setText(e.target.value)} className="w-full mt-12 mb-2 px-2 pt-2 h-10svh"></textarea>
           {error != "" && <div className="pb-4 pl-2 text-red-600">{error}</div>}
           <div className="flex flex-row place-content-between">
-            <button onClick={submit} className="bg-white rounded-lg p-2 hover:bg-slate-200">
+            <button onClick={submit} className="bg-white rounded-lg p-2 hover:bg-slate-200 mt-2">
               Submit Query
             </button>
             <div>
-              <button onClick={reset} className="bg-white rounded-lg p-2 mx-4 hover:bg-slate-200">
+              <button onClick={reset} className="bg-white rounded-lg p-2 mx-2 hover:bg-slate-200 float-right mt-2">
                 Reset Image
               </button>
-              <button onClick={clear} className="bg-white rounded-lg p-2 hover:bg-slate-200">
+              <button onClick={clear} className="bg-white rounded-lg p-2 mx-2 hover:bg-slate-200 float-right mt-2">
                 Clear Image
               </button>
             </div>
