6760f6485acf8891249aa1fc744e711f034a2c45
Ricky Liu <z5207969@ad.unsw.edu.au>
2024-04-16 20:38:37 +1000

    Implement parallel web workers (glitch-effect)
    Saving this version, as the resulting effects are pretty interesting.
    They are a result of batching the image processing to multiple workers
    then appending the end results as they come in, resulting in the image
    parts coming out of order
    
    I suspect there is another issue because the final image has some
    horizontal slicing instead of just vertical slicing. Also there
    are a lot of exceptions.

--- app/Image/imageDraw.tsx
+++ app/Image/imageDraw.tsx
@@ -6,12 +6,16 @@ import { memo, useEffect } from "react";
 export default memo(function ImageDraw({ fileName, command }: { fileName: File|null, command: string}) {
 
   const canvasRef: RefObject<HTMLCanvasElement> = React.createRef();
+  var data = []
 
   useEffect(()=> {
     const canvas = canvasRef.current
     if (canvas && fileName && window.Worker) {
       const ctx = canvas.getContext('2d')
-      const worker = new Worker(new URL("../canvasWorker/worker.ts", import.meta.url))
+
+
+
+
       var image = new Image();
       const offscreen = new OffscreenCanvas(image.width,image.height)
       var offscreenCtx = offscreen.getContext('2d')
@@ -20,18 +24,27 @@ export default memo(function ImageDraw({ fileName, command }: { fileName: File|n
         offscreenCtx.canvas.height = image.height
         offscreenCtx.drawImage(image, 0, 0, image.width, image.height)
         var imageData = offscreenCtx?.getImageData(0,0, image.width, image.height).data
-        console.log(imageData)
-        worker.postMessage({imageData: imageData, command:command})
+        const length = imageData?.length/window.navigator.hardwareConcurrency
+        let currLength = 0
+        for (let i = 0; i < window.navigator.hardwareConcurrency; i++) {
+          let newWorker = new Worker(new URL("../canvasWorker/worker.ts", import.meta.url))
+          newWorker.postMessage({imageData: imageData?.slice(currLength, currLength+length), command:command})
+          newWorker.onmessage = function (e) {
+            data.push(e.data)
+            if (data.length == window.navigator.hardwareConcurrency) {
+              data = Uint8ClampedArray.from(data.reduce((a, b) => [...a, ...b], []));
+              console.log(data)
+              ctx.canvas.width = image.width
+              ctx.canvas.height = image.height
+              var imageData = ctx?.createImageData(image.width, image.height)
+              imageData?.data.set(data)
+              ctx.putImageData(imageData, 0, 0)
+            }
+          }
+          currLength += length
+        }
       }
       image.src = URL.createObjectURL(fileName)
-
-      worker.onmessage = function(e) {
-        ctx.canvas.width = image.width
-        ctx.canvas.height = image.height
-        var imageData = ctx?.createImageData(image.width, image.height)
-        imageData?.data.set(e.data)
-        ctx.putImageData(imageData, 0, 0)
-      }
     }
   })
   return (
